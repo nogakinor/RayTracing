888cb4e5059a253b9b8cc92127a2bd1ad694f0d2
nogakinor <nogakinor@gmail.com>
2022-05-15 23:57:39 +0300

    image is now printed in color, not perfect yet

--- color.py
+++ color.py
@@ -5,9 +5,9 @@ class Color:
 
     # we need to check incorrect rgb values (not in 0-1?)
     def __init__(self, r: float, g: float, b: float):
-        self.r = round(r)
-        self.g = round(g)
-        self.b = round(b)
+        self.r = r
+        self.g = g
+        self.b = b
 
     def plus(self, other):
         return Color(self.r + other.r, self.g + other.g, self.b + other.b)
@@ -19,13 +19,14 @@ class Color:
         return Color(self.r * scalar, self.g * scalar, self.b * scalar)
 
     def get_red(self):
-        return int(255 * self.r)
+        return self.r
 
     def get_green(self):
-        return int(255 * self.g)
+        return self.g
 
     def get_blue(self):
-        return int(255 * self.b)
+        # return 255 * self.b
+        return self.b
 
     def gray_scale(self):
         outSum = self.r + self.g + self.b
